70a672863bbf7e397b7f8a7ec51414c939f4dcc1
Max Base <maxbasecode@gmail.com>
2023-01-17 17:32:46 +0100

    create homepage

---
+++ index.php
@@ -0,0 +1,101 @@
+<?php
+require "_base.php";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Memorial of the great scientist Professor Ali Reza Ashrafi</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    <main>
+        <div class="row">
+            <img class="figure" src="ashrafi.jpg" alt="Professor Ali Reza Ashrafi">
+            <h1>
+                Memorial Of The Great Scientist<br>
+                Professor Ali Reza Ashrafi
+            </h1>
+        </div>
+        <br>
+
+        <h1>Professor Ali Reza Ashrafi</h1>
+        <h2>Department of Pure Mathematics, Faculty of Mathematical Sciences, University of Kashan, Kashan, I. R. Iran</h2>
+        <br>
+        <p>
+            Ali Reza Ashrafi (1964 – 9 January 2023) was an Iranian mathematician who worked in computational group theory and mathematical chemistry. Ashrafi was a professor at the department of pure mathematics of the University of Kashan and the vice president of the International Academy of Mathematical Chemistry.
+        </p>
+
+        <h2>Students</h2>
+        <ul>
+        </ul>
+        <br>
+
+        <h2>Books</h2>
+        <ul>
+            <li>
+                <b>Atlas of ρ, ρE, and TM–EC for Fullerenes Isomers</b>, IGI Global
+                فاطمه کوره پزان مفتخر,سید علیرضا اشرفی قمرودی,اتورینو اوری,میهای پوتز	
+            </li>
+            <li>
+                <b>Distance, Symmetry, and Topology in Carbon Nanomaterials</b>, Springer
+                سید علیرضا اشرفی قمرودی,میرچا دیودی
+            </li>
+            <li>
+                <b>Topological Efficiency Approach to Fullerene Stability - Case Study with C50</b>, Bentam
+                آنته گرائوواج,سید علیرضا اشرفی قمرودی,اتورینو اوری	
+            </li>
+            <li>
+                <b>Fullerenes: Chemistry, Natural Sources and Technological Applications</b>, Nova Publisher -- USA
+                فاطمه کوره پزان مفتخر,سید علیرضا اشرفی قمرودی,اتورینو اوری,میهای وی پوتز	
+            </li>
+            <li>
+                <b>Handbook of Functional Nanomaterials. Volume 3</b>, نوا - آمریکا
+                سید علیرضا اشرفی قمرودی,فاطمه کوره پزان مفتخر
+            </li>
+            <li>
+                <b>Topics in Chemical Graph Theory</b>, University of Kragujevac and Faculty of Science Kr
+                فاطمه کوره پزان مفتخر,سید علیرضا اشرفی قمرودی	
+            </li>
+            <li>
+                <b>Exotic Properties of Carbon Nanomatter</b>, Springer Netherlands
+                فاطمه کوره پزان مفتخر,سید علیرضا اشرفی قمرودی,اتورینو اوری,میهای پوتز
+            </li>
+            <li>
+                <b>Fullerenes: Topology and Symmetry</b>, University of Kragujevac
+                فاطمه کوره پزان مفتخر,سید علیرضا اشرفی قمرودی
+            </li>
+            <li>
+                <b>Topological Modelling of Nanostructures and Extended Systems</b>, Springer
+                سید علیرضا اشرفی قمرودی,Franco Cataldo,علی ایرانمنش,Ottorino Ori
+            </li>
+            <li>
+                <b>Diamond and Related Nanostructures</b>, Springer Netherlands
+                سید علیرضا اشرفی قمرودی,فاطمه کوره پزان مفتخر,میرچا دیودی,مونیکا استفو
+            </li>
+            <li>
+                <b>Topological Modelling of Nanostructures and Extended Systems</b>, Springer Netherlands
+                سید علیرضا اشرفی قمرودی,زینب مهرانیان
+            </li>
+            <li>
+                <b>Topological Modelling of Nanostructures and Extended Systems</b>, Springer Netherlands
+                سید علیرضا اشرفی قمرودی,محمدعلی ایرانمنش,زهرا یاراحمدی
+            </li>
+        </ul>
+        <br>
+
+        <h2>Conference Papers</h2>
+        <ul>
+        </ul>
+        <br>
+
+        <h2>Conference Papers</h2>
+        <ul>
+        </ul>
+        <br>
+
+    </main>
+</body>
+</html>
